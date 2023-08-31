8afbd20242fb5986c2b536b2edaa2cad91ca2415
unknown <meenus0708@gmail.com>
2023-08-31 16:19:20 +0530

    Added

---
+++ siteproject/siteapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from . models import Place
+from . models import People
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1=People.objects.all()
+
+    return render(request,"about.html",{'cd result':obj,'result1':obj1})
